98d0a03f45061bd8d545bd10e6b856e001874ef4
JuniorJacki <me@juniorjacki.de>
2025-06-05 21:37:50 +0200

    Added Tools to GameField

--- src/main/java/de/juniorjacki/game/GameField.java
+++ src/main/java/de/juniorjacki/game/GameField.java
@@ -1,6 +1,8 @@
 package de.juniorjacki.game;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class GameField {
@@ -18,4 +20,58 @@ public class GameField {
     AtomicReference<HashMap<Byte, Byte>> row5 = new AtomicReference<>(new HashMap<Byte, Byte>());
     AtomicReference<HashMap<Byte, Byte>> row6 = new AtomicReference<>(new HashMap<Byte, Byte>());
 
+
+    /**
+     * Gets Value of Field of the GameField
+     * @param rowNum Row Number Counted from Driving Side Left to Right (Starting from Scanner reflektor)
+     * @param columnNum Column Counted Downwards (0 is the Top) , 5 is the Column at the Bottom
+     * @return Optional of Field Value or Optional.empty()
+     */
+    public Optional<Byte> getFieldValue(byte rowNum, byte columnNum) {
+        AtomicReference<HashMap<Byte, Byte>> row = switch (rowNum) {
+            case 0 -> row0;
+            case 1 -> row1;
+            case 2 -> row2;
+            case 3 -> row3;
+            case 4 -> row4;
+            case 5 -> row5;
+            case 6 -> row6;
+            default -> null;
+        };
+        if (row == null) return Optional.empty();
+        return Optional.of(row.get().get(columnNum));
+    }
+
+    /**
+     * Gets Interpreted Color Value of Field of the GameField
+     * @param rowNum Row Number Counted from Driving Side Left to Right (Starting from Scanner reflektor)
+     * @param columnNum Column Counted Downwards (0 is the Top) , 5 is the Column at the Bottom
+     * @return Optional of Color Value or Air for Empty Fields
+     */
+    public Optional<ICSValue> getFieldICSValue(byte rowNum, byte columnNum) {
+        return getFieldValue(rowNum, columnNum).map(ICSValue::getColorByValue).or(() -> Optional.of(ICSValue.AIR));
+    }
+
+    /**
+     * InterpretedColorScannerValue
+     */
+    public enum ICSValue {
+        RED_DROP(40,65 ),
+        YELLOW_DROP(70,100 ),
+        BLUE(20,35 ),
+        AIR(0,  0);
+
+        private final byte min;
+        private final byte max;
+
+        ICSValue(int min, int max) {
+            this.min = (byte)min;
+            this.max = (byte)max;
+        }
+
+        public static ICSValue getColorByValue(Byte colorValue) {
+            return Arrays.stream(values()).filter(iCSValue -> iCSValue.min >= colorValue && iCSValue.max <= colorValue).findFirst().orElse(AIR);
+        }
+
+    }
 }
